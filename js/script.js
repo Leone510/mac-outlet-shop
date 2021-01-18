b5ad111da60d46e19548c855af862dd3e409fc8d
Leone510 <meteorologsio@gmail.com>
2021-01-18 18:28:39 +0200

    HW2 and fix button on card with zero stock

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+//------------------------------------------------------------------
+//------------------ Function for easy code ------------------------
+//------------------------------------------------------------------
+
 const cElem = (tagName, className, text) => {
    const elem = document.createElement(tagName);
    elem.className = className || '';
@@ -21,6 +25,8 @@ const gElem = param => {
 const listContainer = gElem('.cards')
 
 //------------------------------------------------------------------
+//--------------------- Slider -------------------------------------
+//------------------------------------------------------------------
 
 $(document).ready(function() {
    $('.header__slider').slick({
@@ -29,7 +35,7 @@ $(document).ready(function() {
       autoplay: true,
       autoplayspeed: 5000,
       speed: 1000,
-      fade: true,
+      fade: true,      
    });
 });
 
@@ -40,7 +46,7 @@ const bannerUrls = [
    'banners/ipad_air_banner.jpg',
    'banners/iphone_12_banner.jpg',
    'banners/mac_book_banner.jpg',
-   'banners/watch_banner.jpg'
+   'banners/Apple_watch_banner.jpg'
 ];
 
 const renderSlideItem = banner => {
@@ -58,17 +64,24 @@ const renderSlideItem = banner => {
 const elems = bannerUrls.forEach(item => renderSlideItem(item));
 
 //------------------------------------------------------------------
-//-------------------------- Render card ---------------------------
+//-------------------------- Render cards --------------------------
 //------------------------------------------------------------------
 
 const renderCard = device => {
+   const chekedBtn = () => {
+      if (device.orderInfo.inStock === 0) {
+         return 'card__btnDis';
+      } return 'card__btn';
+   }
+
    const container = cElem('div', 'card');
+   container.id = device.id;
    const innerCard = {
       img: cElem('img', 'card__img'),
       title: cElem('h2', 'card__title', device.name),
       onStock: cElem('p', 'card__stock', `${device.orderInfo.inStock} left in stock`),
       price: cElem('p', 'card__price', `Price: ${device.price} $`),
-      btnAddToCart: cElem('button', 'card__btn', 'Add to cart...'),
+      btnAddToCart: cElem('button', chekedBtn(), 'Add to cart...'),
       statistic: cElem('div', 'card__stats'),
    }
    innerCard.img.src = `img/${device.imgUrl}`;
@@ -78,6 +91,9 @@ const renderCard = device => {
    statsRight.innerHTML = `<p>${device.price}<br/>orders</p>`
    innerCard.statistic.append(statsLeft, statsRight);
    container.append(...(Object.values(innerCard)));
+   container.addEventListener('click', e => {
+      renderModalWindow(e.currentTarget);
+   }, true)
    return container;
 }
 
@@ -88,6 +104,52 @@ const renderCards = list => {
 
 renderCards(items);
 
+//------------------------------------------------------------------
+//--------------------- Modal window for card ----------------------
+//------------------------------------------------------------------
+
+const renderModalWindow = (card) => {
+   const device = items.find(item => item.id == card.id);
+   const modalWindowCard = gElem('.modalWindowCard');
+   modalWindowCard.classList.add('visible');
+   const container = cElem('div', 'modalWindowCard__container');
+   const containerLeft = cElem('div', 'modalWindowCard__container__left');
+   const containerMid = cElem('div', 'modalWindowCard__container__mid');
+   const containerRight = cElem('div', 'modalWindowCard__container__right');
+   const img = cElem('img', 'modalWindowCard__container__left__img');
+   img.src = `img/${device.imgUrl}`;
+   modalWindowCard.addEventListener('click', e => {
+      e.stopPropagation();
+      modalWindowCard.classList.remove('visible');
+   })
+   containerLeft.append(img);
+
+   const deviceInfo = {
+      name: cElem('h2', 'modalWindowCard__container__mid__title', device.name),
+      stats: cElem('div', 'card__stats'),
+      color: cElem('p', 'modalWindowCard__container__mid__color', `Color: ${device.color}`),
+      os: cElem('p', 'modalWindowCard__container__mid__os', `Operation System: ${device.os}`),
+      chip: cElem('p', 'modalWindowCard__container__mid__chip', `chip: ${device.chip.name}`),
+      itemHeight: cElem('p', 'modalWindowCard__container__mid__height', `Height: ${device.size.height}`),
+      itemWidth: cElem('p', 'modalWindowCard__container__mid__width', `Width: ${device.size.width} cm`),
+      itemDepth: cElem('p', 'modalWindowCard__container__mid__depth', `Depth: ${device.size.depth} cm`),
+      itemWeight: cElem('p', 'modalWindowCard__container__mid__weight', `Weight: ${device.size.weight} kg`),
+   }
+   deviceInfo.stats.innerHTML = card.querySelector('.card__stats').innerHTML;
+   containerMid.append(...(Object.values(deviceInfo)));
+
+   const price = cElem('p', 'modalWindowCard__container__right__price', `$ ${device.price}`);
+   const stock = cElem('p', 'modalWindowCard__container__right__stock', `Stock: ${device.orderInfo.inStock} pcs.`);
+   const btn = cElem('button', 'card__btn', 'Add to cart...');
+   containerRight.append(price, stock, btn);
+
+   container.append(containerLeft, containerMid, containerRight);
+   modalWindowCard.clear().append(container);
+}
+
+//------------------------------------------------------------------
+//-------------------------- Filters -------------------------------
+//------------------------------------------------------------------
 
 class Filter {
    constructor(){
@@ -128,6 +190,135 @@ const filter = new Filter();
 
 gElem('#deviceInp').oninput = (e) => filter.filterByName(e.target.value);
 gElem('#sortDevices').onchange = (e) => filter.sortItems(e.target.value);
+
+//------------------------------------------------------------------
+//-------------------------- Aside filters -------------------------
+//------------------------------------------------------------------
+
+class Utils {
+   constructor() {
+      this.colors = this._getColors();
+      this.categories = this._getCategory();
+      this.priceRange = this._getPriceRange();
+   }
+
+   _getColors() {
+      let result = [];
+      items.forEach(item => {
+         item.color.forEach(col => {
+            result.includes(col) || result.push(col)
+         })
+      })
+      return result;
+   }
+
+   _getCategory() {
+      let result = [];
+      items.forEach(item => {
+         result.includes(item.category) || result.push(item.category)
+      })
+      return result;
+   }
+
+   _getPriceRange() {
+      const sortByAsc = [...items]
+         .sort((a, b) => a.price - b.price);
+      return {
+         from: sortByAsc[0].price,
+         to: sortByAsc[sortByAsc.length - 1].price,
+      }
+   }
+}
+
+utils = new Utils();
+
+class AsideFilter {
+   constructor() {
+      this.filtersArr = [
+         {
+            type: 'range',
+            title: 'Price',
+            variant: utils.priceRange,
+         },
+         {
+            type: 'check',
+            title: 'Colors',
+            variants: utils.colors,
+         },
+         {
+            type: 'check',
+            title: 'Categories',
+            variants: utils.categories,
+         }
+      ]
+   }
+
+   get contentRenderMethod() {
+      return {
+         check: this._renderContenCheck,
+         range: this._renderContenRange,
+      }
+   }
+
+   _renderCategory(item) {
+      const container = cElem('div', 'filterItem');
+      const title = cElem('div', 'filterItem__title');
+      title.innerHTML = `
+         <span>${item.title}</span>
+         <div class="arrow"></div>
+      `
+      title.onclick = function () {
+         this.classList.toggle('filterItem__title--active');
+         const content = this.parentElement.children[1];
+         content.classList.toggle('filterItem__content--active');
+      }
+      const content = cElem('div', 'filterItem__content');
+      const chooseMethod = this.contentRenderMethod[item.type];
+      const filterContent = chooseMethod(item);
+      content.append(...filterContent);
+      container.append(title, content);
+      return container;
+   }
+
+   renderFilters() {
+      const container = gElem('.filterContainer');
+      const filters = this.filtersArr.map(item => this._renderCategory(item));
+      container.innerHTML = '';
+      container.append(...filters);
+      
+   }
+
+   _renderContenCheck(item) {
+      return item.variants.map(variant => {
+         const title = cElem('span', null, variant);
+         const label = cElem('label');
+         const inp = cElem('input');
+         inp.type = 'checkbox';
+         label.append(inp, title);
+         return label;
+      })
+   }
+
+   _renderContenRange(item) {
+      const containerFrom = cElem('div');
+      const labelFrom = cElem('label', null, 'From');
+      const inputFrom = cElem('input');
+      inputFrom.value = item.variant.from
+      containerFrom.append(labelFrom, inputFrom);
+
+      const containerTo = cElem('div');
+      const labelTo = cElem('label', null, 'To');
+      const inputTo = cElem('input');
+      inputTo.value = item.variant.to
+      containerTo.append(labelTo, inputTo);
+
+      return [containerFrom, containerTo];
+   }
+
+}
+
+const asideFilter = new AsideFilter();
+asideFilter.renderFilters();
 
 
 
